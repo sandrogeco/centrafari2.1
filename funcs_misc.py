449705216c770badccd74275959bc6f09980d2c8
sandrogeco <sandro@geco-company.com>
2025-10-27 12:07:12 +0100

    correzion calib ctr 5

--- funcs_misc.py
+++ funcs_misc.py
@@ -16,17 +16,18 @@ def preprocess(image, cache):
     crop_w = config['crop_w']
     crop_h = config['crop_h']
     start_y = max(int(crop_center[1] - crop_h/2), 0)
-    end_y = min(int(start_y + crop_h/2),2*crop_h)
+    end_y = int(start_y + crop_h)
     start_x = max(int(crop_center[0] - crop_w/2), 0)
-    end_x = min(int(start_x + crop_w/2),2*crop_w)
+    end_x = int(start_x + crop_w)
 
     start_y1 = max(int(crop_center[1] - crop_h), 0)
-    end_y1 = min(int(start_y1 + crop_h),2*crop_h)
+    end_y1 = min(int(start_y1 + crop_h),config['height'])
     start_x1 = max(int(crop_center[0] - crop_w), 0)
-    end_x1 = min(int(start_x1 + 2*crop_w),2*crop_w)
-    image_o = image.copy() * 0
-    image = image[start_y1: end_y1, start_x1:end_x1]
+    end_x1 = int(start_x1 + 2*crop_w)
 
+
+    image = image[start_y: end_y,start_x1:end_x1]
+    image_o = image.copy() * 0
     image_o[start_y : end_y, start_x : end_x] = image[start_y : end_y, start_x : end_x]
     image=cv2.convertScaleAbs(image, alpha=1.0, beta=20)
 
